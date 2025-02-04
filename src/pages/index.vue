009862b2a8906a7042d94cfe0aa67b569f3cdcfa
Kayel2 <gulayjunkyle@gmail.com>
2025-02-04 20:59:15 +0800

    fix: made it responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="background">
     <v-container>
-      <v-row class="mb-10">
-        <v-col cols="12" lg="8">
-          <h1 class="text mx-auto">To detect and identify plant pests</h1>
+      <!-- Heading -->
+      <v-row class="mb-6 mb-sm-10">
+        <v-col cols="12" lg="8" md="10" sm="12">
+          <h1 class="text mx-auto text-sm-left">
+            To detect and identify plant pests
+          </h1>
         </v-col>
       </v-row>
     </v-container>
 
-    <v-container style="margin-top: 200px;">
+    <!-- Buttons -->
+    <v-container style="margin-top: 29vh">
       <v-row justify="center">
         <v-btn class="button-background mt-4" rounded="lg" color="black">
           <span class="button">Sign In</span>
         </v-btn>
       </v-row>
       <v-row justify="center">
-        <RouterLink to="RegisterView" style="text-decoration: none" class="mt-1"
-          ><span class="router">Create an Account</span></RouterLink
+        <RouterLink
+          to="RegisterView"
+          style="text-decoration: none"
+          class="mt-1"
         >
+          <span class="router">Create an Account</span>
+        </RouterLink>
       </v-row>
     </v-container>
   </div>
@@ -40,7 +48,7 @@
 .text {
   font-family: "Montserrat", sans-serif !important;
   font-weight: 700;
-  font-size: 55px;
+  font-size: 3rem; /* Responsive font size */
   color: #ffffff;
   margin-top: 50px;
 }
@@ -48,7 +56,7 @@
 .button {
   font-family: "Roboto", sans-serif !important;
   font-weight: 500;
-  font-size: 20px;
+  font-size: 1.25rem; 
   color: #ffffff;
   text-transform: none !important;
 }
@@ -56,14 +64,27 @@
 .router {
   font-family: "Roboto", sans-serif !important;
   font-weight: 400;
-  font-size: 15px;
+  font-size: 1rem; 
   color: #ffffff;
   text-transform: none !important;
 }
 
 .button-background {
   opacity: 0.7;
-  width: 296px;
+  width: 296px; 
   height: 52px;
+}
+
+
+@media (max-width: 600px) {
+  .text {
+    font-size: 2.5rem; 
+  }
+  .button {
+    font-size: 1rem; 
+  }
+  .router {
+    font-size: 0.875rem; 
+  }
 }
 </style>
