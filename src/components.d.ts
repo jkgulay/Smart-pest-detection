82989e1d0d6f0a97991f4089976688f05bb20574
Kayel2 <gulayjunkyle@gmail.com>
2025-02-08 14:34:38 +0800

    try2

--- src/components.d.ts
+++ src/components.d.ts
@@ -17,6 +17,7 @@ declare module 'vue' {
     LoginForm: typeof import('./components/auth/LoginForm.vue')['default']
     LoginView: typeof import('./components/views/auth/LoginView.vue')['default']
     RegisterForm: typeof import('./components/auth/RegisterForm.vue')['default']
+    RegisterView: typeof import('./components/views/auth/RegisterView.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     ScanHistory: typeof import('./components/views/ScanHistory.vue')['default']
